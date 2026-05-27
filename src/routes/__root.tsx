9111c1e4fcadd06bfb3f3014df401ec8d9b5954b
Zumgler <zuminfinitix@gmail.com>
2026-05-27 14:59:54 -0300

    favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -79,6 +79,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
     ],
   }),
   shellComponent: RootShell,
